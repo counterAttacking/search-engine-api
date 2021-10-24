da118cb5fa51e12bf930bedc863dc57ced1d86a1
unknown <ptjh0309@hanmail.net>
2021-10-24 12:28:26 +0900

    Modify process of showing url not server but database

--- src/controller/search.controller.ts
+++ src/controller/search.controller.ts
@@ -3,6 +3,7 @@ import { KMR } from "koalanlp/API";
 import { Tagger } from "koalanlp/proc";
 import { Keyword } from '../models/Keyword';
 import { Op } from 'sequelize';
+import sequelize from 'sequelize';
 import { Link } from '../models/Link';
 
 const router = express.Router();
@@ -45,43 +46,67 @@ router.get('/', async (req, res) => {
     /* 분석이 된 단어들을 Database에 존재하는지 찾고
     찾은 데이터의 Link를 사용자에게 전달
     */
-    const keywords = await Keyword.findAll({
-        where: {
-            name: {
-                [Op.in]: morphemeKeywords,
-            },
-        },
+    // const keywords = await Keyword.findAll({
+    //     where: {
+    //         name: {
+    //             [Op.in]: morphemeKeywords,
+    //         },
+    //     },
+    //     include: [
+    //         {
+    //             model: Link,
+    //         },
+    //     ],
+    // });
+
+    // // 검색 순위를 정렬하기 위해서는 우선 검색 결과에 따른 URL을 counting
+    // const frequentLink = new Map<string, FrequentLink>();
+    // keywords.forEach((keyword) => {
+    //     keyword.links.forEach((link) => {
+    //         const exist = frequentLink.get(link.url);
+    //         if (exist) {
+    //             exist.count += 1;
+    //             frequentLink.set(link.url, exist);
+    //         } else {
+    //             frequentLink.set(link.url, {
+    //                 url: link.url,
+    //                 content: link.description,
+    //                 count: 1,
+    //             });
+    //         }
+    //     });
+    // });
+
+    // // counting한 결과를 정렬
+    // const result = Array.from(frequentLink.values()).sort(
+    //     (link1, link2) => link2.count - link1.count
+    // );
+
+    // return res.status(200).json(result);
+
+    const links = await Link.findAll({
         include: [
             {
-                model: Link,
+                model: Keyword,
+                where: {
+                    name: {
+                        [Op.in]: Array.from(searchKeywords.values()),
+                    },
+                },
+                attributes: [],
+                through: {
+                    attributes: [],
+                },
             },
         ],
+        attributes: [
+            "url",
+            [sequelize.fn("count", sequelize.col("Link.url")), "total"],
+        ],
+        group: ["Link.id"],
     });
 
-    // 검색 순위를 정렬하기 위해서는 우선 검색 결과에 따른 URL을 counting
-    const frequentLink = new Map<string, FrequentLink>();
-    keywords.forEach((keyword) => {
-        keyword.links.forEach((link) => {
-            const exist = frequentLink.get(link.url);
-            if (exist) {
-                exist.count += 1;
-                frequentLink.set(link.url, exist);
-            } else {
-                frequentLink.set(link.url, {
-                    url: link.url,
-                    content: link.description,
-                    count: 1,
-                });
-            }
-        });
-    });
-
-    // counting한 결과를 정렬
-    const result = Array.from(frequentLink.values()).sort(
-        (link1, link2) => link2.count - link1.count
-    );
-
-    return res.status(200).json(result);
+    return res.status(200).json(links);
 });
 
 export default router;
